3601801349b439da031d10db8c7bbbc9469afe81
DevSplinter <danieldrzazga.94@gmail.com>
2017-11-11 00:22:47 +0100

    Coloring page

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let figureBox = document.querySelector('.figure-box');
 let red = 0, green = 0, blue = 0;
 let colorExample = document.querySelector('.color-example');
 
+//input[type=range]::-webkit-slider-thumb
+
 inputsColor.forEach(input => input.addEventListener('input', function(e){ 
     switch(e.target.id){
         case 'red': red = e.target.value;
+//            e.parentNode. querySelector('input[type=range]::-webkit-slider-thumb').style.backgroundColor = `rgb(${red},0,0)`;
+            //console.log(window.getComputedStyle(e.target.parentNode.querySelector('input[type=range]'), '::-webkit-slider-thumb').backgroundColor = 'rgb(0,0,0)')
             break;
         case 'green': green = e.target.value;
             break;
