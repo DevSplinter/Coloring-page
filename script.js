ab7dbf9a8db117a2526af49f1a42c95f5a5563f3
DevSplinter <danieldrzazga.94@gmail.com>
2017-11-13 09:15:09 +0100

    Coloring page

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
 let inputsColor = document.querySelectorAll('.inputColor');
 let figureBox = document.querySelector('.figure-box');
-let red = 0, green = 0, blue = 0;
+let red = 0,
+    green = 0,
+    blue = 0;
 let colorExample = document.querySelector('.color-example');
 
-//input[type=range]::-webkit-slider-thumb
-
-inputsColor.forEach(input => input.addEventListener('input', function(e){ 
-    switch(e.target.id){
-        case 'red': red = e.target.value;
-//            e.parentNode. querySelector('input[type=range]::-webkit-slider-thumb').style.backgroundColor = `rgb(${red},0,0)`;
-            //console.log(window.getComputedStyle(e.target.parentNode.querySelector('input[type=range]'), '::-webkit-slider-thumb').backgroundColor = 'rgb(0,0,0)')
+inputsColor.forEach(input => input.addEventListener('input', function (e) {
+    switch (e.target.id) {
+        case 'red':
+            red = e.target.value;
             break;
-        case 'green': green = e.target.value;
+        case 'green':
+            green = e.target.value;
             break;
-        case 'blue': blue = e.target.value;
-            break; 
-                         }
+        case 'blue':
+            blue = e.target.value;
+            break;
+    }
     colorExample.style.backgroundColor = `rgb(${red},${green},${blue})`;
 }));
 
 
-function makeFigures(){
+function makeFigures() {
     let setCounter = document.querySelector('#counter');
     let fragment = document.createDocumentFragment();
-    
 
-  setCounter.addEventListener('mousemove', function(){
+
+    setCounter.addEventListener('mousemove', function () {
         let setCounterValue = setCounter.value;
         let figureBoxCounter = figureBox.childElementCount;
-        
+
         let makeElement = setCounterValue - figureBoxCounter;
-        
-      if(makeElement > 0){
-      for(let i=0; i < makeElement; i++){
-          let newFigure = document.createElement('div');
-            newFigure.classList.add('figure');
-            fragment.appendChild(newFigure);
-      }
-        figureBox.appendChild(fragment);
-          }else if(makeElement < 0){
-              figureBox.removeChild(figureBox.lastChild);
-          }
+
+        if (makeElement > 0) {
+            for (let i = 0; i < makeElement; i++) {
+                let newFigure = document.createElement('div');
+                newFigure.classList.add('figure');
+                fragment.appendChild(newFigure);
+            }
+            figureBox.appendChild(fragment);
+        } else if (makeElement < 0) {
+            figureBox.removeChild(figureBox.lastChild);
+        }
     });
 }
-  
-function selectFigure(){
-    figureBox.addEventListener('click', function(e){
+
+function selectFigure() {
+    figureBox.addEventListener('click', function (e) {
         let figures = document.querySelectorAll('.figure');
-        if(e.target.className == 'figure'){
-        e.srcElement.style.backgroundColor = `rgb(${red},${green},${blue})`;
-            }
-    })   
+        if (e.target.className == 'figure') {
+            e.srcElement.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        }
+    })
 }
 
 makeFigures();
